7d4ae3bdb2cb90aaeb7fbc1ccc436eec1eca5ae9
kuidoli <kuidoli@hanmail.net>
2022-03-22 23:57:58 +0900

    handling error

---
+++ nestjs/transactions-queryrunner/src/banks/common/exceptions/http.exception.ts
@@ -0,0 +1,26 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const message = exception['message'];
+    console.log(exception);
+
+    response.status(status).json({
+      isError: true,
+      message: message,
+      statusCode: status,
+      data: exception,
+    });
+  }
+}
---
+++ nestjs/transactions-queryrunner/src/banks/common/interceptors/success-response.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        isError: false,
+        message: data['message'],
+        statusCode: data['statusCode'],
+        data: data,
+      })),
+    );
+  }
+}
