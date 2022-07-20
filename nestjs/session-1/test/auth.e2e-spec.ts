71891d2dc2d0e2af9663c961e8c4378c73e9f52e
kuidoli <kuidoli@hanmail.net>
2022-07-20 23:50:55 +0900

    update auth e2e test

--- nestjs/session-1/test/auth.e2e-spec.ts
+++ nestjs/session-1/test/auth.e2e-spec.ts
@@ -5,6 +5,21 @@ import { sessionOptions } from '../src/config/db-sessions.config';
 import * as request from 'supertest';
 import * as session from 'express-session';
 import { AuthModule } from '../src/auth/auth.module';
+import { AuthService } from '../src/auth/auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserAccount } from '../src/entities/userAccount.entity';
+import { ExpressSessions } from '../src/entities/expressSessions.entity';
+import { Repository } from 'typeorm';
+import { AuthController } from '../src/auth/auth.controller';
+
+const mockUsersRepository = () => ({
+  save: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockSessionsRepository = () => ({});
+
+type MockRepository<T> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
@@ -12,8 +27,12 @@ describe('AuthController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AuthModule],
-      providers: [],
-    }).compile();
+    })
+      .overrideProvider(getRepositoryToken(UserAccount))
+      .useValue(mockUsersRepository)
+      .overrideProvider(getRepositoryToken(ExpressSessions))
+      .useValue(mockSessionsRepository)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     app.use(session(sessionOptions));
@@ -21,6 +40,7 @@ describe('AuthController (e2e)', () => {
   });
 
   it('/signUp (POST)', () => {
-    return request(app.getHttpServer()).post('/signUp').expect(200);
+    // return request(app.getHttpServer()).post('/signUp').expect(200);
+    return request(app.getHttpServer()).post('/signUp').expect(500);
   });
 });
