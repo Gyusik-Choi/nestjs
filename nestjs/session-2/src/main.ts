b2aa6e161f7412fa4beefe7664b7ed79f9954790
kuidoli <kuidoli@hanmail.net>
2022-10-04 23:30:04 +0900

    session and passport initial setting

--- nestjs/session-2/src/main.ts
+++ nestjs/session-2/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.use(
+    session({
+      secret: configService.get('SESSION_SECRET'),
+      resave: false,
+      saveUninitialized: true,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
 bootstrap();
