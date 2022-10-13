0b38ab4de8c598c62955838a072a19e13c47c3e0
kuidoli <kuidoli@hanmail.net>
2022-10-13 23:38:33 +0900

    create signOut

--- nestjs/session-2/src/auth/auth.controller.ts
+++ nestjs/session-2/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDataDTO } from './dto/signUpData.dto';
 import SignInInterface from './interfaces/signIn.interface';
@@ -27,5 +35,34 @@ export class AuthController {
   @UseGuards(AuthenticationGuard)
   async authenticate(@Req() req: AuthenticationInterface) {
     return req.user;
+  }
+
+  @Post('signOut')
+  @UseGuards(AuthenticationGuard)
+  async signOut(@Req() req: AuthenticationInterface) {
+    // 참고한 소스코드에는 logOut 에 콜백 함수를 안 적어도 괜찮았는데
+    // passport 버전의 차이로
+    // index.d.ts 의 logout, logOut 구현이 달랐다
+    // 참고한 소스코드에서 바라보는 passport 모듈의
+    // index.d.ts 의 logout, logOut 구현은 아래와 같다 (소문자 o, 대문자 O 주의!)
+    // =>
+    // logout(): void;
+    // logOut(): void;
+    //
+    // 이 프로젝트에서 바라보는 passport 모듈의
+    // index.d.ts 의 logout, logOut 구현은 아래와 같다
+    // =>
+    // logout(options: { keepSessionInfo?: boolean }, done: (err: any) => void): void;
+    // logout(done: (err: any) => void): void;
+    // logOut(options: { keepSessionInfo?: boolean }, done: (err: any) => void): void;
+    // logOut(done: (err: any) => void): void;
+
+    // https://www.passportjs.org/concepts/authentication/logout/
+    req.logOut((err) => {
+      if (err) {
+        throw new InternalServerErrorException();
+      }
+    });
+    req.session.cookie.maxAge = 0;
   }
 }
