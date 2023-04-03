51ee030e3c069a1b5a5874bd5275a28c46d328b5
최 규식 <kuidoli@hanmail.net>
2023-04-03 10:34:42 +0900

    create db-config module

--- projects/pagination-1/src/app.module.ts
+++ projects/pagination-1/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { DbConfigModule } from './db-config/db-config.module';
 
 @Module({
-  imports: [BoardModule],
+  imports: [BoardModule, DbConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
