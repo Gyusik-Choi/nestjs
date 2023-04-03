90c6de7f68f15b4031a96eec0f1c7753479b171b
최 규식 <kuidoli@hanmail.net>
2023-04-03 10:29:35 +0900

    add board entity to module

--- projects/pagination-1/src/board/board.module.ts
+++ projects/pagination-1/src/board/board.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from '../../src/entities/board.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Board])],
   controllers: [BoardController],
   providers: [BoardService]
 })
