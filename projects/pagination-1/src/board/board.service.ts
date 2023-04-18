4d11365853297220e32282fc89e41345395aa429
최 규식 <kuidoli@hanmail.net>
2023-04-19 00:05:35 +0900

    change return data by using dto

--- projects/pagination-1/src/board/board.service.ts
+++ projects/pagination-1/src/board/board.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { BoardSearchRequestDTO } from './dto/boardSearchRequest.dto';
 import { BoardRepository } from '../repositories/board.repository';
 import { Board } from '../entities/board.entity';
+import { BoardSearchArticleDTO } from './dto/boardSearchArticle.dto';
+import { Post } from './dto/post';
 
 @Injectable()
 export class BoardService {
@@ -9,7 +11,8 @@ export class BoardService {
     private readonly boardRepository: BoardRepository,
   ) {}
 
-  async search(queryParam: BoardSearchRequestDTO) {
+  async search(queryParam: BoardSearchRequestDTO): Promise<Post<BoardSearchArticleDTO>> {
     const boards: [Board[], number] = await this.boardRepository.paging(queryParam);
+    return new Post<BoardSearchArticleDTO>(boards[1], queryParam.getLimit(), boards[0].map(v => new BoardSearchArticleDTO(v)));
   }
 }
