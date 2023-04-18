ab5bdbb76ff1895b23e6fb7bc84a899c69a7cf19
최 규식 <kuidoli@hanmail.net>
2023-04-19 00:08:49 +0900

    set dependency of unit test of board service

--- projects/pagination-1/src/board/board.service.spec.ts
+++ projects/pagination-1/src/board/board.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardService } from './board.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { BoardRepository } from '../repositories/board.repository';
+
+const mockBoardRepository = () => ({});
 
 describe('BoardService', () => {
   let service: BoardService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardService],
+      providers: [
+        BoardService,
+        {
+          provide: getRepositoryToken(BoardRepository),
+          useValue: mockBoardRepository(),
+        }
+      ],
     }).compile();
 
     service = module.get<BoardService>(BoardService);
