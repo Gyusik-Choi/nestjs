fa3eb83863c71ed312b770e1fef7e46572f8dbd4
kuidoli <kuidoli@hanmail.net>
2023-04-23 23:09:59 +0900

    follow prettier suggestion

--- projects/pagination-1/src/board/board.controller.ts
+++ projects/pagination-1/src/board/board.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BoardService } from './board.service';
 import { BoardSearchRequestDTO } from './dto/boardSearchRequest.dto';
 
 @Controller('board')
 export class BoardController {
-  constructor(
-    private readonly boardService: BoardService,
-  ) {}
+  constructor(private readonly boardService: BoardService) {}
 
   @Get('search')
   @UsePipes(new ValidationPipe({ transform: true }))
--- projects/pagination-1/src/board/board.service.ts
+++ projects/pagination-1/src/board/board.service.ts
@@ -7,12 +7,18 @@ import { Post } from './dto/post';
 
 @Injectable()
 export class BoardService {
-  constructor(
-    private readonly boardRepository: BoardRepository,
-  ) {}
+  constructor(private readonly boardRepository: BoardRepository) {}
 
-  async search(queryParam: BoardSearchRequestDTO): Promise<Post<BoardSearchArticleDTO>> {
-    const boards: [Board[], number] = await this.boardRepository.paging(queryParam);
-    return new Post<BoardSearchArticleDTO>(boards[1], queryParam.getLimit(), boards[0].map(v => new BoardSearchArticleDTO(v)));
+  async search(
+    queryParam: BoardSearchRequestDTO,
+  ): Promise<Post<BoardSearchArticleDTO>> {
+    const boards: [Board[], number] = await this.boardRepository.paging(
+      queryParam,
+    );
+    return new Post<BoardSearchArticleDTO>(
+      boards[1],
+      queryParam.getLimit(),
+      boards[0].map((v) => new BoardSearchArticleDTO(v)),
+    );
   }
 }
