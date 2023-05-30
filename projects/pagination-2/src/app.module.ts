03c99dcd35925c6c2526a27ed3408ee876444d97
최 규식 <kuidoli@hanmail.net>
2023-05-30 14:15:32 +0900

    Feat: connect to mysql

--- projects/pagination-2/src/app.module.ts
+++ projects/pagination-2/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { DbConfigModule } from './db-config/db-config.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbConfigService } from './db-config/db-config.service';
 
 @Module({
-  imports: [BoardModule, DbConfigModule],
+  imports: [
+    BoardModule, 
+    DbConfigModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [DbConfigModule],
+      useClass: DbConfigService,
+      inject: [DbConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- projects/pagination-2/src/db-config/db-config.service.ts
+++ projects/pagination-2/src/db-config/db-config.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist';
+import { Board } from 'src/entities/board.entity';
 
 @Injectable()
-export class DbConfigService {}
+export class DbConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(connectionName?: string): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DATABASE_HOST'),
+      port: parseInt(this.configService.get<string>('DATABASE_PORT')),
+      username: this.configService.get<string>('DATABASE_USERNAME'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+      database: this.configService.get<string>('DATABASE_DATABASE'),
+      entities: [Board],
+      logging: true,
+    }
+  }
+}
