82ca94605f639b1e365595659165e2cac2332c70
최 규식 <kuidoli@hanmail.net>
2023-06-14 17:56:53 +0900

    Feat: add search method

--- projects/pagination-2/src/board/board.controller.ts
+++ projects/pagination-2/src/board/board.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { BoardSearchRequestDTO } from './dto/board-search-request.dto';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(
+    private readonly boardService: BoardService,
+  ) {}
+
+  @Get('search')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async search(
+    @Param() req: BoardSearchRequestDTO,
+  ) {
+    return await this.boardService.search(req);
+  }
+}
