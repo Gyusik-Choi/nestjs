116ef3ac1fe066106e65f25182a8b46475ca4b1b
최 규식 <kuidoli@hanmail.net>
2023-06-14 16:51:18 +0900

    Feat: add board module

--- projects/pagination-2/src/board/board.module.ts
+++ projects/pagination-2/src/board/board.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
+import { TypeORMCustomModule } from '../../src/common/modules/typeorm-custom.module';
+import { BoardRepository } from '../../src/repositories/board.repository';
 
 @Module({
+  imports: [
+    TypeORMCustomModule.forCustomRepository([BoardRepository])
+  ],
   controllers: [BoardController],
   providers: [BoardService]
 })
